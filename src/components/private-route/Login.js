710c093cbf9c6bf545667c29af0f885a0dc310f2
trịnh bảo long <baolong551@gmail.com>
2022-11-06 18:40:10 +0700

    fix css

--- src/components/private-route/Login.js
+++ src/components/private-route/Login.js
@@ -1,5 +1,31 @@
+import { MailOutlined, PhoneOutlined, UserOutlined } from "@ant-design/icons";
+
 export default function Login({ isAuthenticated, setIsAuthenticated }) {
   // cho nay la de lam logic check coi co authen khong
 
-  return <>Login</>;
+  return (
+    <>
+      <section className="contact" id="contact">
+        <div className="row">
+          <form action="">
+            <h3>Đăng nhập</h3>
+            <div className="inputBox">
+              <span>
+                <UserOutlined />
+              </span>
+              <input type="text" placeholder="Username" />
+            </div>
+            <div className="inputBox">
+              <span>
+                <MailOutlined />
+              </span>
+              <input type="password" placeholder="Password" />
+            </div>
+            
+            <input type="submit" value="đăng nhập" className="btn" />
+          </form>
+        </div>
+      </section>
+    </>
+  );
 }
