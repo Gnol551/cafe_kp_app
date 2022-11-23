49c67c45d752bc724e0dad10e7fa543c544ad8fa
trịnh bảo long <baolong551@gmail.com>
2022-11-23 21:49:10 +0700

    make logout

--- src/components/admin/dashboard/Header.jsx
+++ src/components/admin/dashboard/Header.jsx
@@ -1,11 +1,36 @@
 import { UserOutlined } from "@ant-design/icons";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
+  let navigate = useNavigate();
+
+  const signOut = (e) => {
+    e.preventDefault();
+    // 1/ xoa token
+    localStorage.removeItem("token");
+    // 2/ chuyen qua trang login
+    navigate("/login");
+  };
+
   return (
     <header>
       <img src="../../img/logo.png" alt="logo" className="logodash" />
       <span className="namedash">CAFENA</span>
-      <UserOutlined className="user-dash"/>
+      <UserOutlined
+        onClick={() => {
+          setOpen(!open);
+        }}
+        className="user-dash"
+      />
+      {open === true ? (
+        <div className="open-logout">
+          <button onClick={signOut}>Đăng xuất</button>
+        </div>
+      ) : (
+        <></>
+      )}
     </header>
   );
 }
